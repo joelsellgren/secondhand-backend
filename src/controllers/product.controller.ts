dafb46483fe3e02a9020f530bdf14192db49b9a6
joelsellgren <joel.sellgren@hotmail.com>
2025-01-09 16:17:30 +0100

    create not done

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -3,7 +3,27 @@ import Product from '../models/Product'
 
 require('dotenv').config()
 
-export const createProduct = async () => {}
+export const createProduct = async (req: Request, res: Response) => {
+  try {
+    const product = await Product.create({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      size: req.body.size,
+      category: req.body.category,
+      imageUrl: req.body.imageUrl,
+    })
+
+    console.log(product) // Log the new new product
+
+    res.status(201).json()
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    })
+  }
+}
 export const getAllProducts = async () => {}
 export const getProduct = async () => {}
 export const updateProduct = async () => {}
